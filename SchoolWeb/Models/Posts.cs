c1f6606690c3fa4444e6a0ef376ea921a2070b1c
Maskop <Maskop@DESKTOP-BS5JN0Q>
2023-04-12 20:40:21 +0200

    Add project files.

---
+++ SchoolWeb/Models/Posts.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolWeb.Models
+{
+    public class Posts
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = "";
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+    }
+}
